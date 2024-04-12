947d9c3f1ebca1b6037a7f5ed99eeefcf072c729
vaishusbabu <vaishusbabu@gmail.com>
2024-04-12 16:03:53 +0530

    initial commit

---
+++ src/Components/EditStudent.js
@@ -0,0 +1,128 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+
+function EditStudent() {
+    const [userData, setUserData] = useState(null);
+
+
+    useEffect(() => {
+        const loggedInUserEmail = localStorage.getItem('loggedInUserEmail');
+        const allStudentData = JSON.parse(localStorage.getItem('studentFormData')) || [];
+
+        console.log("loggedInUserEmail", loggedInUserEmail);
+        console.log("allStudentData", allStudentData);
+
+        const user = allStudentData.find(student => student.email === loggedInUserEmail);
+
+        if (user) {
+            setUserData(user);
+        } else {
+            console.log('User not found');
+        }
+    }, []);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const allStudentData = JSON.parse(localStorage.getItem('studentFormData')) || [];
+        const updatedUserData = allStudentData.map(student => {
+            if (student.email === userData.email) {
+                return { ...student, ...userData };
+            }
+            return student;
+        });
+
+        localStorage.setItem('studentFormData', JSON.stringify(updatedUserData));
+        console.log('Data saved successfully');
+        console.log(updatedUserData);
+        alert('Updated Successfully');
+    }
+
+
+
+    return (
+        <div>
+            <div className='navbar'>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container-fluid">
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <Link className="nav-link active" aria-current="page" to="/studenthome">Home</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link active" aria-current="page" to="/editstd">Update Profile</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link active" aria-current="page" to="/passwordstd">Password Change</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div className='formContainer'>
+                <div className='formWrapper'>
+                    <h6>Edit Profile</h6>
+                    <form onSubmit={handleSubmit}>
+                        {userData && (
+                            <div>
+                                <div className="mb-3">
+                                    <label htmlFor="name" className="form-label">Name:</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="name"
+                                        value={userData.name}
+                                        onChange={(e) => setUserData({ ...userData, name: e.target.value })}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="form-label">Email:</label>
+                                    <input
+                                        readOnly
+                                        type="email"
+                                        className="form-control"
+                                        id="email"
+                                        value={userData.email}
+                                        onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="mobileNumber" className="form-label">Mobile Number:</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="mobileNumber"
+                                        value={userData.mobileNumber}
+                                        onChange={(e) => setUserData({ ...userData, mobileNumber: e.target.value })}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="manager" className="form-label">Manager:</label>
+                                    <input
+                                        readOnly
+                                        type="text"
+                                        className="form-control"
+                                        id="manager"
+                                        value={userData.manager}
+                                        onChange={(e) => setUserData({ ...userData, manager: e.target.value })}
+                                    />
+                                </div>
+
+                                <button type="submits" className="btn btn-success">
+                                    Submit
+                                </button>
+                            </div>
+                        )}
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default EditStudent
