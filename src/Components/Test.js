69c2087bdd606aee2e23d0780a64b32433ca7fde
Vaishnavi S Babu <vaishusbabu@gmail.com>
2024-04-14 14:00:44 +0530

    Update Test.js

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -1,142 +1,142 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
 
-function Register() {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: '',
-        mobileNumber: '',
-        userType: ' ',
-        manager: ''
-    });
+// function Register() {
+//     const [formData, setFormData] = useState({
+//         name: '',
+//         email: '',
+//         password: '',
+//         mobileNumber: '',
+//         userType: ' ',
+//         manager: ''
+//     });
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const { userType, ...data } = formData;
-        let localStorageKey = '';
-        let storedData = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+//     const handleInputChange = (e) => {
+//         const { name, value } = e.target;
+//         setFormData({
+//             ...formData,
+//             [name]: value
+//         });
+//     };
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         const { userType, ...data } = formData;
+//         let localStorageKey = '';
+//         let storedData = JSON.parse(localStorage.getItem(localStorageKey)) || [];
 
-        if (userType === 'student') {
-            localStorageKey = 'studentFormData';
-            storedData = JSON.parse(localStorage.getItem(localStorageKey)) || [];
-            storedData.push(data);
-        } else if (userType === 'manager') {
-            localStorageKey = 'managerFormData';
-            storedData = JSON.parse(localStorage.getItem(localStorageKey)) || [];
-            storedData.push(data);
-        } else {
-            localStorageKey = 'userFormData';
-            storedData = JSON.parse(localStorage.getItem(localStorageKey)) || [];
-            storedData.push(data);
-        }
+//         if (userType === 'student') {
+//             localStorageKey = 'studentFormData';
+//             storedData = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+//             storedData.push(data);
+//         } else if (userType === 'manager') {
+//             localStorageKey = 'managerFormData';
+//             storedData = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+//             storedData.push(data);
+//         } else {
+//             localStorageKey = 'userFormData';
+//             storedData = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+//             storedData.push(data);
+//         }
 
-        localStorage.setItem(localStorageKey, JSON.stringify(storedData));
-        console.log(`Form data saved to localStorage (${userType}):`, storedData);
-    };
+//         localStorage.setItem(localStorageKey, JSON.stringify(storedData));
+//         console.log(`Form data saved to localStorage (${userType}):`, storedData);
+//     };
 
-    const managerData = JSON.parse(localStorage.getItem('managerFormData'));
-    console.log('Manager Data:', managerData);
-    return (
-        <div>
-            <div className='navbar'>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <div className="container-fluid">
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/adminhome">Home</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link active" aria-current="page" to="/viewalluser">View All Users</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/register">Registration</Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-            </div>
+//     const managerData = JSON.parse(localStorage.getItem('managerFormData'));
+//     console.log('Manager Data:', managerData);
+//     return (
+//         <div>
+//             <div className='navbar'>
+//                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//                     <div className="container-fluid">
+//                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//                             <span className="navbar-toggler-icon"></span>
+//                         </button>
+//                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//                                 <li className="nav-item">
+//                                     <Link className="nav-link active" aria-current="page" to="/adminhome">Home</Link>
+//                                 </li>
+//                                 <li className="nav-item">
+//                                     <Link className="nav-link active" aria-current="page" to="/viewalluser">View All Users</Link>
+//                                 </li>
+//                                 <li className="nav-item">
+//                                     <Link className="nav-link" to="/register">Registration</Link>
+//                                 </li>
+//                             </ul>
+//                         </div>
+//                     </div>
+//                 </nav>
+//             </div>
 
-            <div className='formContainer'>
-                <div className='formWrapper'>
-                    <span className='title' logo>Registration Page </span>
-                    <form onSubmit={handleSubmit}>
-                        <div className="mb-3">
-                            <input type="text"
-                                className="form-control"
-                                placeholder='Name'
-                                aria-describedby="emailHelp"
-                                name="name"
-                                value={formData.name}
-                                onChange={handleInputChange} />
-                        </div>
-                        <div className="mb-3">
-                            <input type="email"
-                                className="form-control"
-                                placeholder='Email'
-                                aria-describedby="emailHelp"
-                                name="email"
-                                value={formData.email}
-                                onChange={handleInputChange} />
-                        </div>
-                        <div className="mb-3">
-                            <input type="password"
-                                className="form-control"
-                                placeholder='Password'
-                                aria-describedby="emailHelp"
-                                name="password"
-                                value={formData.password}
-                                onChange={handleInputChange} />
-                        </div>
-                        <div className="mb-3">
-                            <input type="text"
-                                className="form-control"
-                                placeholder='Mobile Number'
-                                aria-describedby="emailHelp"
-                                name="mobileNumber"
-                                value={formData.mobileNumber}
-                                onChange={handleInputChange} />
-                        </div>
-                        <div className="mb-3">
-                            <select className="form-select" aria-label="User Type" name="userType" value={formData.userType} onChange={handleInputChange}>
-                                <option value="user">User</option>
-                                <option value="student">Student</option>
-                                <option value="manager">Manager</option>
-                            </select>
-                        </div>
-                        {formData.userType === 'student' && (
-                            <div className="mb-3">
-                                <select className="form-select" aria-label="Manager" name="manager" value={formData.manager} onChange={handleInputChange}>
-                                    <option >Select Manager</option>
-                                    {managerData.map((manager, index) => (
+//             <div className='formContainer'>
+//                 <div className='formWrapper'>
+//                     <span className='title' logo>Registration Page </span>
+//                     <form onSubmit={handleSubmit}>
+//                         <div className="mb-3">
+//                             <input type="text"
+//                                 className="form-control"
+//                                 placeholder='Name'
+//                                 aria-describedby="emailHelp"
+//                                 name="name"
+//                                 value={formData.name}
+//                                 onChange={handleInputChange} />
+//                         </div>
+//                         <div className="mb-3">
+//                             <input type="email"
+//                                 className="form-control"
+//                                 placeholder='Email'
+//                                 aria-describedby="emailHelp"
+//                                 name="email"
+//                                 value={formData.email}
+//                                 onChange={handleInputChange} />
+//                         </div>
+//                         <div className="mb-3">
+//                             <input type="password"
+//                                 className="form-control"
+//                                 placeholder='Password'
+//                                 aria-describedby="emailHelp"
+//                                 name="password"
+//                                 value={formData.password}
+//                                 onChange={handleInputChange} />
+//                         </div>
+//                         <div className="mb-3">
+//                             <input type="text"
+//                                 className="form-control"
+//                                 placeholder='Mobile Number'
+//                                 aria-describedby="emailHelp"
+//                                 name="mobileNumber"
+//                                 value={formData.mobileNumber}
+//                                 onChange={handleInputChange} />
+//                         </div>
+//                         <div className="mb-3">
+//                             <select className="form-select" aria-label="User Type" name="userType" value={formData.userType} onChange={handleInputChange}>
+//                                 <option value="user">User</option>
+//                                 <option value="student">Student</option>
+//                                 <option value="manager">Manager</option>
+//                             </select>
+//                         </div>
+//                         {formData.userType === 'student' && (
+//                             <div className="mb-3">
+//                                 <select className="form-select" aria-label="Manager" name="manager" value={formData.manager} onChange={handleInputChange}>
+//                                     <option >Select Manager</option>
+//                                     {managerData.map((manager, index) => (
 
-                                        <option key={index} value={manager.name}>{manager.name}</option>
-                                    ))}
-                                </select>
+//                                         <option key={index} value={manager.name}>{manager.name}</option>
+//                                     ))}
+//                                 </select>
 
-                            </div>
-                        )}
-                        <button type="submit" className="btn btn-primary">Submit</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    );
-}
+//                             </div>
+//                         )}
+//                         <button type="submit" className="btn btn-primary">Submit</button>
+//                     </form>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
 
-export default Register;
+// export default Register;
 
 
 
